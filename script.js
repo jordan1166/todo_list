ebcfbd48319c68fcde3ac3c86bcabcd31ac8cdad
Jordan <jtate1166@gmail.com>
2022-01-04 12:16:06 -0500

    Add ability to delete todo tasks from list

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 // Even listeners
 todoButton.addEventListener("click", addToDo);
+todoList.addEventListener("click", deleteCheck);
 
 // Functions
 function addToDo(event) {
@@ -32,4 +33,19 @@ function addToDo(event) {
   todoList.appendChild(todoDiv);
   // Clear todo input value
   todoInput.value = "";
+}
+
+function deleteCheck(event) {
+  // Item will either be the trash or check button
+  const item = event.target;
+  // Delete todo
+  if (item.classList[0] === "trash-button") {
+    item.parentElement.remove();
+  }
+
+  // Mark tasks as completed
+  if (item.classList[0] === "complete-button") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
