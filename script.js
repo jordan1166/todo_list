ebd7949106bc9f3665306aa78ba116b0e098f380
Jordan <jtate1166@gmail.com>
2022-01-04 10:54:08 -0500

    Add functionality for creating new 'todo' tasks

---
+++ script.js
@@ -0,0 +1,33 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Even listeners
+todoButton.addEventListener("click", addToDo);
+
+// Functions
+function addToDo(event) {
+  // prevent form from submitting
+  event.preventDefault();
+  // Todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // Create li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  // Check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-button");
+  todoDiv.appendChild(completedButton);
+  // Check trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("complete-button");
+  todoDiv.appendChild(trashButton);
+  // Append to list
+  todoList.appendChild(todoDiv);
+}
